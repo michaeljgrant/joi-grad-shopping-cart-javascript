1ea1f34f9799e6c3641914baca3edbb7f4c467a7
michaeljgrant <mjg.grant.m@gmail.com>
2021-09-03 16:34:50 +1000

    Save Point

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import Product from "./model/Product.js";
 import Customer from "./model/Customer.js";
 
 const products = [
-    new Product(10.0, "DIS_10_PRODUCT1", "Product 1"),
-    new Product(180.0, "DIS_15_PRODUCT2", "Product 2")
+  new Product(10.0, "DIS_10_PRODUCT1", "Product 1"),
+  new Product(180.0, "DIS_15_PRODUCT2", "Product 2"),
 ];
 
 const customer = new Customer("John Doe");
@@ -12,9 +12,11 @@ const customer = new Customer("John Doe");
 const shoppingCart = new ShoppingCart(customer, products);
 const productThree = new Product(30.0, "PRODUCT3", "Product 3");
 shoppingCart.addProduct(productThree);
+const ProductFour = new Product(200.0, "DIS_20_PRODUCT4", "Product 4");
+shoppingCart.addProduct(ProductFour);
 
-console.log(shoppingCart.displaySummary())
+console.log(shoppingCart.displaySummary());
 
 const order = shoppingCart.checkout();
 
-console.log(order.displaySummary())
+console.log(order.displaySummary());
--- src/model/ShoppingCart.js
+++ src/model/ShoppingCart.js
@@ -2,43 +2,51 @@ import _ from "lodash";
 import Order from "./Order.js";
 
 export default class ShoppingCart {
-    constructor(customer, products) {
-        this.customer = customer;
-        this.products = products;
-    };
+  constructor(customer, products) {
+    this.customer = customer;
+    this.products = products;
+  }
 
-    addProduct = (product) => {
-        this.products.push(product);
-    };
+  addProduct = (product) => {
+    this.products.push(product);
+  };
 
-    removeProduct = (product) => {
-        _.remove(this.products, product);
-    };
+  removeProduct = (product) => {
+    _.remove(this.products, product);
+  };
 
-    checkout = () => {
-        let totalPrice = 0;
-        let loyaltyPointsEarned = 0;
+  checkout = () => {
+    let totalPrice = 0;
+    let loyaltyPointsEarned = 0;
 
-        this.products.forEach(product => {
-            let discount = 0;
-            if (product.code.startsWith("DIS_10")) {
-                discount = product.price * 0.1;
-                loyaltyPointsEarned += product.price / 10;
-            } else if (product.code.startsWith("DIS_15")) {
-                discount = product.price * 0.15;
-                loyaltyPointsEarned += product.price / 15;
-            } else {
-                loyaltyPointsEarned += product.price / 5;
-            }
+    this.products.forEach((product) => {
+      let discount = 0;
+      if (product.code.startsWith("DIS_10")) {
+        discount = product.price * 0.1;
+        loyaltyPointsEarned += product.price / 10;
+      } else if (product.code.startsWith("DIS_15")) {
+        discount = product.price * 0.15;
+        loyaltyPointsEarned += product.price / 15;
+      } else if (product.code.startsWith("DIS_20")) {
+        discount = product.price * 0.2;
+        loyaltyPointsEarned += product.price / 20;
+      } else {
+        loyaltyPointsEarned += product.price / 5;
+      }
 
-            totalPrice += product.price - discount;
-        });
+      totalPrice += product.price - discount;
+    });
 
-        return new Order(totalPrice, loyaltyPointsEarned);
-    };
+    return new Order(totalPrice, loyaltyPointsEarned);
+  };
 
-    displaySummary = () =>  {
-        return "Customer: " + this.customer.name + "\n" + 
-            "Bought:  \n" + this.products.map(p => "- " + p.name + ", " + p.price).join('\n');
-    }
-};
+  displaySummary = () => {
+    return (
+      "Customer: " +
+      this.customer.name +
+      "\n" +
+      "Bought:  \n" +
+      this.products.map((p) => "- " + p.name + ", " + p.price).join("\n")
+    );
+  };
+}
